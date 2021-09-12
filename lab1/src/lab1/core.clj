ac02070f5d371dc78560d2baf0e08ee2b43c9bfe
Dmitriy Linevich <d.linevich@eastbanctech.ru>
2021-09-12 19:09:53 +0700

    Lab 1-1(v1)

---
+++ lab1/src/lab1/core.clj
@@ -0,0 +1,32 @@
+(ns lab1.core)
+
+(defn add-alf [word alf]
+  (if (> (count alf) 0)
+    (if (some #{(first alf)} word)
+      (add-alf word (rest alf))
+      (cons (cons (first alf) word) (add-alf word (rest alf)))
+      )
+    '()
+    )
+  )
+
+(defn add-words [words alf]
+  (if (empty? words)
+    '()
+    (concat (add-alf (first words) alf) (add-words (rest words) alf))
+    )
+  )
+
+(defn write-words [n symbols]
+  (if (> n 0)
+    (if (> n 1)
+      (add-words (write-words (dec n) symbols) symbols)
+      (add-alf '() symbols)
+      )
+    '()
+    )
+  )
+
+
+
+
